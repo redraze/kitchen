492c7c21ada33544af9665cd8aac243b07d3ac56
redraze <coross3@outlook.com>
2023-02-13 11:34:40 -0800

    Update how fridge objects are imported into scene.

--- components/Fridge.tsx
+++ components/Fridge.tsx
@@ -1,7 +1,6 @@
+import { Vector3, Euler } from "three";
 import { useGLTF } from "@react-three/drei";
-import { useRef } from "react";
-import { Vector3 } from "three";
-import { Euler } from "three";
+import { useMemo } from "react";
 
 export type FridgeProps = {
     position: Vector3
@@ -13,28 +12,13 @@ export type FridgeProps = {
 
 export default function Fridge(props: FridgeProps) {
     const url = "objects/fridge.gltf"
-    // TS 'any' usage here              ---------------------------------
-    const { nodes, materials }: any = useGLTF<string | string[]>(url);
-    const ref: any = useRef<THREE.Group>();
+    const { scene } = useGLTF(url);
 
     return(
-        <group
+        <primitive
             {...props}
-            ref={ref}
             dispose={null}
-        >
-            {Object.keys(nodes).map((element, key) => {
-                return <mesh 
-                    castShadow
-                    receiveShadow
-                    geometry={nodes[element].geometry}
-                    material={materials['Material.001']}
-                    rotation={nodes[element].rotation}
-                    scale={nodes[element].scale}
-                    position={nodes[element].position}
-                    key={key}
-                />
-            })}
-        </group>
+            object={useMemo(() => scene.clone(), [scene])}
+        ></primitive>
     );
 };
