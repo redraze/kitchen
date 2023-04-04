b97fe665c8c6ee7035fe3c4ef92791ecb2f1c4e8
redraze <coross3@outlook.com>
2023-04-04 10:40:39 -0700

    Raise control group position and rotation state up to scene component. Pass focus state control down to ingredient navbar.

--- components/HUD Components/IngredientsNav.tsx
+++ components/HUD Components/IngredientsNav.tsx
@@ -1,36 +1,63 @@
-import { numStateType } from "lib/commonPropTypes";
 import { useState } from "react";
 import css from "styles/Ingredients.module.scss";
 import Button from "./Button";
+import { componentSettings, fridgeSettings, initSettings, pantrySettings } from "lib/componentSettings";
 
 type IngredientsNavProps = {
-    focusState: numStateType
     ingredients: JSX.Element[]
+    focus: number
+    changeSettings: (params: componentSettings) => void
 };
 
-export default function IngredientsNav({ focusState, ingredients }: IngredientsNavProps) {
+export default function IngredientsNav(
+    { 
+        ingredients, 
+        focus, 
+        changeSettings 
+    }: IngredientsNavProps
+) {
     const [open, setOpen] = useState(false);
-    let refrigeratedMap: JSX.Element[] = [];
-    let nonRefrigeratedMap: JSX.Element[] = [];
-    // let spicesMap: JSX.Element[] = [];
+    let fridgeMap: JSX.Element[] = [];
+    let pantryMap: JSX.Element[] = [];
     ingredients.map((ingredient: JSX.Element) => {
         ingredient.props.ingredient.refrigerated === true ? 
-            refrigeratedMap = [...refrigeratedMap, ingredient] :
-            nonRefrigeratedMap = [...nonRefrigeratedMap, ingredient]
+            fridgeMap = [...fridgeMap, ingredient] :
+            pantryMap = [...pantryMap, ingredient]
     });
+
+    const toggleSettings = (settings: componentSettings) => {
+        if (focus === settings.focus) {
+            changeSettings(initSettings);
+        } else {
+            changeSettings(settings);
+        };
+    };
 
     return(
         <div className={ css.ingredientsNav } style={{right: open ? '80%' : '100%'}}>
             <Button openState={{bool: open, setBool: setOpen}} left/>
-            <div className={ css.refrigerated }>
-                {refrigeratedMap}
-            </div>
-            <div className={ css.nonRefrigerated }>
-                {nonRefrigeratedMap}
-            </div>
-            {/* <div className={ css.spices }>
-                {spicesMap}
-            </div> */}
+            <ul>
+                <li className={
+                    focus === fridgeSettings.focus ?
+                    css.fridge_focus :
+                    css.fridge
+                }
+                onClick={() => toggleSettings(fridgeSettings)}
+                >
+                    <span>Refridgerated Ingredients</span>
+                    <div>{fridgeMap}</div>
+                </li>
+                <li className={
+                    focus === pantrySettings.focus ?
+                    css.pantry_focus :
+                    css.pantry
+                }
+                onClick={() => toggleSettings(pantrySettings)}
+                >
+                    <span>Non-refrigerated Ingredients</span>
+                    <div>{pantryMap}</div>
+                </li>
+            </ul>
         </div>
     );
 };
