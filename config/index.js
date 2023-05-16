b0280adc19e7847502ac767eae32f444dd610001
redraze <coross3@outlook.com>
2023-05-16 16:01:43 -0700

    update server const in config/index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV === 'production';
 export const server = dev ? 
-    'https://kitchen-lilac.vercel.app/' :
-    'http://localhost:5000';
+    'https://kitchen-git-test-redraze.vercel.app/' :
+    'http://localhost:3000';
