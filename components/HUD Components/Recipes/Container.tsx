5b40f8f22aa9a7812f49013f9d34215cf73bbabe
redraze <coross3@outlook.com>
2023-07-24 16:23:24 -0700

    Add filter and close buttons to the recipe data container.

--- components/HUD Components/Recipes/Container.tsx
+++ components/HUD Components/Recipes/Container.tsx
@@ -1,12 +1,12 @@
 import type { ApolloError } from '@apollo/client';
 import type { RecipeDataTypeOutput } from 'lib/typeDefsExports';
 import type { stateType, filterType } from 'lib/commonTypes';
+import { useEffect, useState } from 'react';
 import css from 'styles/HUD/Recipes/Container.module.scss';
 import RecipeDataCard from './Card';
 import Spinner from './Spinner';
 import Error from './Error';
 import Filters from './Filters';
-import { useEffect, useState } from 'react';
 
 type RecipeDataContainerProps = {
     clientRecipeData: object
@@ -36,7 +36,7 @@ export default function RecipeDataContainer(
             return temp;
         });
     }, [data]);
-        
+
     const dataMap = () => {
         if (!error && loading) {
             return <Spinner />
@@ -60,6 +60,9 @@ export default function RecipeDataContainer(
         return <Error />;
     };
 
+    const [filterDisplay, setFilterDisplay] = useState(false);
+    useEffect(() => { setFilterDisplay(false) }, [recipeDataVisibility.value]);
+
     return (
         <div 
             style={ 
@@ -71,35 +74,54 @@ export default function RecipeDataContainer(
         >
             <div 
                 className={ css.screen }
-                onClick={ () => 
-                    recipeDataVisibility.setValue(!recipeDataVisibility.value)
-                }
+                onClick={ () => recipeDataVisibility.setValue(!recipeDataVisibility.value) }
             ></div>
             <div
-                className={ css.wrapper }
+                className={ css.container }
                 style={
                     error || loading || !recipeDataVisibility.value ?
                         { width: '140px', height: '140px', transition: 'width 0s, height 0s' } :
                         { width: '80%', height: '90vh', transition: 'width 0.5s, height 0.5s' }
                 }
             >
-                <div className={ css.filters }>
-                    { 
-                        !error && !loading && data ?
+                <button
+                    onClick={ () => recipeDataVisibility.setValue(false) }
+                    className={ css.closeButton }
+                    style={ error || loading || !recipeDataVisibility.value ? { display: 'none' } : {} }
+                >
+                    <img src={'/CloseIcon.png'} alt='X' />
+                </button>
+                <div
+                    className={
+                        !filterDisplay ?
+                            css.filtersWrapper :
+                            [css.filtersWrapper, css.filtersWrapperActive].join(' ')
+                    }
+                    style={ error || loading || !recipeDataVisibility.value ? { display: 'none' } : {} }
+                >
+                    <button 
+                        onClick={ () => setFilterDisplay(!filterDisplay) } 
+                        className={ css.filterButton }
+                    >
+                        <img src={'/FilterIcon.png'} alt='Filters:' />
+                    </button>
+                    <div style={{ display: 'flex' }}>
+                        {
                             Object.entries(filters).map((item, idx) => {
                                 return (
-                                    <Filters 
-                                        key={idx} 
-                                        filter={item[0]} 
+                                    <Filters
+                                        key={idx}
+                                        filter={item[0]}
                                         catagories={item[1]}
                                         filterState={{ value: filters, setValue: setFilters}}
+                                        active={filterDisplay}
                                     />
                                 );
-                            }) :
-                            <></>
-                    }
+                            })
+                        }
+                    </div>
                 </div>
-                <div className={ css.container }>
+                <div className={ css.results }>
                     { dataMap() }
                 </div>
             </div>
--- components/HUD Components/Recipes/Filters.tsx
+++ components/HUD Components/Recipes/Filters.tsx
@@ -1,28 +1,46 @@
-import type { catagoryType, stateType, filterType } from "lib/commonTypes"
+import type { catagoryType, stateType, filterType } from "lib/commonTypes";
+import { useEffect, useState } from "react";
+import css from 'styles/HUD/Recipes/Filters.module.scss';
 
 type filtersType = {
     filter: string,
     catagories: catagoryType
     filterState: stateType<filterType>
+    active: boolean
 }
 
-export default function Filters({ filter, catagories, filterState }: filtersType) {
-    const handler = (catagory: string) => {
-        // TODO:    toggle state for clicked filter
+export default function Filters({ filter, catagories, filterState, active }: filtersType) {
+    const toggleFilter = (catagory: string) => {
+        let temp = filterState.value;
+        const value = temp[filter as keyof object][catagory as keyof object];
+        // @ts-ignore
+        temp[filter as keyof object][catagory as keyof object] = !value;
+        filterState.setValue(temp);
     };
 
-    return (<>
-        <span>{ filter }</span>
-        <ul>
-            {
-                Object.entries(catagories).map((item, idx) => {
-                    return (
-                        <li key={idx} onClick={ () => handler(item[0]) }>
-                            { item[0] }
-                        </li>
-                    );
-                })
-            }
-        </ul>
-    </>);
+    const [dropDown, setDropDown] = useState(false);
+    useEffect(() => { setDropDown(false) }, [active]);
+
+    return (
+        <div
+            className={ css.wrapper }
+            style={
+                active ?
+                { opacity: 1, marginTop: '0px' } :
+                { opacity: 0, marginTop: '10px' }
+            }>
+            <button onClick={ () => setDropDown(!dropDown) } >{ filter }</button>
+            <ul style={ !dropDown ? {display: 'none'} : {} }>
+                {
+                    Object.entries(catagories).map((item, idx) => {
+                        return (
+                            <li key={idx} onClick={ () => toggleFilter(item[0]) }>
+                                { item[0] }
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        </div>
+    );
 };
